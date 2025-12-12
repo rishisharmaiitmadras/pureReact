7bbe4b92656032194cb0d3ee83c300b5bacf9988
Rishi Sharma <rishisharmaopportunities09@gmail.com>
2025-12-12 11:07:13 +0530

    Context api project

---
+++ 08_minicontext/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import UserContextProvider from './context/UserContextProvider'
+
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+
+      <h1>CONTEXT API</h1>
+
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
